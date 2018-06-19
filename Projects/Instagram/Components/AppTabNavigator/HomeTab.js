0336985e587b5dfe121215ca466a141fc9b50789
tanakorn0314 <tanakorn0412@gmail.com>
2018-06-19 22:12:31 +0700

    CardComponent Created

--- Projects/Instagram/Components/AppTabNavigator/HomeTab.js
+++ Projects/Instagram/Components/AppTabNavigator/HomeTab.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { View, StyleSheet,Text } from 'react-native';
-import {Icon} from 'native-base'
+import CardComponent from '../CardComponent'
+import {
+    Icon,
+    Container,
+    Content,
+} from 'native-base'
 
 export default class HomeTab extends Component {
    
@@ -12,17 +17,19 @@ export default class HomeTab extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <Text>HomeTab</Text>
-            </View>
+            <Container style={styles.container}>
+                <Content>
+                    <CardComponent image="1" likes="123"/>
+                    <CardComponent image="2" likes="100"/>
+                    <CardComponent image="3" likes="20"/>
+                </Content>
+            </Container>
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: 'white',
     }
 })
